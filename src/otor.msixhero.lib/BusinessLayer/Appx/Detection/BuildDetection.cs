d4ee3c15ebff98fc62c5fa0bc8280a0242ad9a03
Marcin Otorowski <marcin.otorowski+git@gmail.com>
2020-01-10 01:27:42 +0100

    Detection of RayPack

--- src/otor.msixhero.lib/BusinessLayer/Appx/Detection/BuildDetection.cs
+++ src/otor.msixhero.lib/BusinessLayer/Appx/Detection/BuildDetection.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Xml;
@@ -14,6 +16,16 @@ namespace otor.msixhero.lib.BusinessLayer.Appx.Detection
 
         public BuildInfo Detect(string appxManifestPath)
         {
+            if (appxManifestPath == null)
+            {
+                throw new ArgumentNullException(nameof(appxManifestPath));
+            }
+
+            if (!File.Exists(appxManifestPath))
+            {
+                throw new FileNotFoundException("The manifest file does not exist.");
+            }
+
             var xmlDocument = new XmlDocument();
             xmlDocument.Load(appxManifestPath);
 
@@ -53,6 +65,11 @@ namespace otor.msixhero.lib.BusinessLayer.Appx.Detection
             }
 
             if (this.DetectVisualStudio(buildValues, out buildInfo))
+            {
+                return buildInfo;
+            }
+
+            if (this.DetectRayPack(buildValues, manifestDirectory, out buildInfo))
             {
                 return buildInfo;
             }
@@ -159,6 +176,57 @@ namespace otor.msixhero.lib.BusinessLayer.Appx.Detection
             }
 
             return true;
+        }
+
+        private bool DetectRayPack(Dictionary<string, string> buildValues, string manifestDirectory, out BuildInfo buildInfo)
+        {
+            buildInfo = null;
+
+            if (buildValues == null || !buildValues.Any())
+            {
+                // Detect RayPack by taking a look at the metadata of PsfLauncher.
+                var fileLauncher = Path.Combine(manifestDirectory, "PsfLauncher.exe");
+                if (File.Exists(fileLauncher))
+                {
+                    var fvi = FileVersionInfo.GetVersionInfo(fileLauncher);
+                    if (fvi.ProductName != null &&  fvi.ProductName.StartsWith("Raynet", StringComparison.OrdinalIgnoreCase))
+                    {
+                        var pv = fvi.ProductVersion;
+                        buildInfo = new BuildInfo
+                        {
+                            ProductName = "RayPack " + Version.Parse(pv).ToString(2),
+                            ProductVersion = fvi.ProductVersion
+                        };
+
+                        return true;
+                    }
+                }
+            }
+            else
+            {
+                // Detect RayPack 6.2 which uses build meta data like this:
+                // <build:Item Name="OperatingSystem" Version="6.2.9200.0" /><build:Item Name="Raynet.RaySuite.Common.Appx" Version="6.2.5306.1168" /></build:Metadata>
+                var rayPack = GetValue(buildValues, "Raynet.RaySuite.Common.Appx");
+                if (rayPack != null)
+                {
+                    buildInfo = new BuildInfo
+                    {
+                        ProductName = "RayPack " + Version.Parse(rayPack).ToString(2), 
+                        ProductVersion = rayPack
+                    };
+
+                    var os = GetValue(buildValues, "OperatingSystem");
+                    if (os != null)
+                    {
+                        var win10Version = Windows10Parser.GetOperatingSystemFromNameAndVersion(os);
+                        buildInfo.OperatingSystem = win10Version.ToString();
+                    }
+
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         private static string GetValue(Dictionary<string, string> dict, string name)
