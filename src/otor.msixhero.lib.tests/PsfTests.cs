8064cb36abc088566b734cf5db4337582aa85abb
Marcin Otorowski <marcin.otorowski+git@gmail.com>
2020-01-30 00:15:08 +0100

    WIP: Psf entities

---
+++ src/otor.msixhero.lib.tests/PsfTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using otor.msixhero.lib.Domain.Appx.Psf;
+
+namespace otor.msixhero.lib.tests
+{
+    [TestFixture]
+    public class PsfTests
+    {
+        [Test]
+        public void TestDeserialization()
+        {
+            var jsonString = @"{""applications"": [
+                {
+                    ""id"": ""App"",
+                    ""executable"": ""ClearbyteMSIXSample/ClearbyteMSIXSample.exe"",
+                    ""workingDirectory"": ""ClearbyteMSIXSample/""
+                }
+                ],
+                ""processes"": [
+                {
+                    ""executable"": ""ClearbyteMSIXSample"",
+                    ""fixups"": [
+                    {
+                        ""dll"": ""FileRedirectionFixup32.dll"",
+                        ""config"": {
+                            ""redirectedPaths"": {
+                                ""packageRelative"": [
+                                {
+                                    ""base"": ""ClearbyteMSIXSample/"",
+                                    ""patterns"": [
+                                    "".*\\.json""
+                                        ]
+                                }
+                                ]
+                            }
+                        }
+                    }
+                    ]
+                }
+                ]
+            }";
+
+            var obj = new PsfConfigSerializer().Deserialize(jsonString);
+        }
+    }
+}
