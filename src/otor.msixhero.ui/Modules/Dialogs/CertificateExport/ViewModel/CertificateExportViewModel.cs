7eb88157b9864544620ffd07c5ebb3b7390f4f2e
Marcin Otorowski <marcin.otorowski+git@gmail.com>
2020-07-15 00:07:58 +0200

    Certificate export was not properly elevated when extracting to the store + there is now a shield icon indicating whether the app will run as admin

--- src/otor.msixhero.ui/Modules/Dialogs/CertificateExport/ViewModel/CertificateExportViewModel.cs
+++ src/otor.msixhero.ui/Modules/Dialogs/CertificateExport/ViewModel/CertificateExportViewModel.cs
@@ -2,6 +2,8 @@
 using System.Threading;
 using System.Threading.Tasks;
 using otor.msixhero.lib.BusinessLayer.Managers.Signing;
+using otor.msixhero.lib.BusinessLayer.State;
+using otor.msixhero.lib.Domain.Commands.Packages.Signing;
 using otor.msixhero.lib.Infrastructure;
 using otor.msixhero.lib.Infrastructure.Progress;
 using otor.msixhero.lib.Infrastructure.SelfElevation;
@@ -14,12 +16,14 @@ namespace otor.msixhero.ui.Modules.Dialogs.CertificateExport.ViewModel
 {
     public class CertificateExportViewModel : ChangeableDialogViewModel
     {
+        private readonly IApplicationStateManager stateManager;
         private readonly ISelfElevationManagerFactory<ISigningManager> signingManagerFactory;
 
-        private ChangeableContainer customValidationContainer;
+        private readonly ChangeableContainer customValidationContainer;
 
-        public CertificateExportViewModel(ISelfElevationManagerFactory<ISigningManager> signingManagerFactory, IInteractionService interactionService) : base("Extract certificate", interactionService)
+        public CertificateExportViewModel(IApplicationStateManager stateManager, ISelfElevationManagerFactory<ISigningManager> signingManagerFactory, IInteractionService interactionService) : base("Extract certificate", interactionService)
         {
+            this.stateManager = stateManager;
             this.signingManagerFactory = signingManagerFactory;
 
             this.InputPath = new ChangeableFileProperty(interactionService)
@@ -39,12 +43,18 @@ namespace otor.msixhero.ui.Modules.Dialogs.CertificateExport.ViewModel
 
             this.SaveToFile = new ChangeableProperty<bool>(true);
             this.SaveToStore = new ChangeableProperty<bool>(true);
+            this.SaveToStore.ValueChanged += this.SaveToStoreOnValueChanged;
 
             customValidationContainer = new ChangeableContainer(this.SaveToFile, this.SaveToStore);
             customValidationContainer.CustomValidation += this.CustomCheckboxValidation;
 
             this.AddChildren(this.InputPath, this.OutputPath, customValidationContainer);
             this.SetValidationMode(ValidationMode.Silent, true);
+        }
+
+        private void SaveToStoreOnValueChanged(object? sender, ValueChangedEventArgs e)
+        {
+            this.OnPropertyChanged(nameof(IsAdminRequired));
         }
 
         private void CustomCheckboxValidation(object sender, ContainerValidationArgs e)
@@ -74,19 +84,21 @@ namespace otor.msixhero.ui.Modules.Dialogs.CertificateExport.ViewModel
 
         public ChangeableProperty<bool> SaveToStore { get; }
 
+        public bool IsAdminRequired => this.SaveToStore.CurrentValue;
+
         protected override async Task<bool> Save(CancellationToken cancellationToken, IProgress<ProgressData> progress)
         {
-            var manager = await this.signingManagerFactory.Get(SelfElevationLevel.HighestAvailable, cancellationToken).ConfigureAwait(false);
-
             if (this.SaveToFile.CurrentValue)
             {
+                var manager = await this.signingManagerFactory.Get(SelfElevationLevel.AsInvoker, cancellationToken).ConfigureAwait(false);
                 await manager.ExtractCertificateFromMsix(this.InputPath.CurrentValue, this.OutputPath.CurrentValue, cancellationToken, progress).ConfigureAwait(false);
                 cancellationToken.ThrowIfCancellationRequested();
             }
 
             if (this.SaveToStore.CurrentValue)
             {
-                await manager.ExtractCertificateFromMsix(this.InputPath.CurrentValue, cancellationToken, progress).ConfigureAwait(false);
+                var command = new TrustPublisher(this.InputPath.CurrentValue);
+                await this.stateManager.CommandExecutor.ExecuteAsync(command, cancellationToken, progress).ConfigureAwait(false);
             }
 
             return true;
