80ae68d1e91b75ec9c95a4d9105233315e795f97
Marcin Otorowski <marcin.otorowski+git@gmail.com>
2020-10-30 09:09:28 +0100

    Add dummy WPF window to handle authentication

---
+++ src/Otor.MsixHero.DeviceGuardLoginHelper/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using Otor.MsixHero.Appx.Signing.DeviceGuard;
+
+namespace Otor.MsixHero.DeviceGuardLoginHelper
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var dgss = new DgssTokenCreator();
+            dgss.SignIn(CancellationToken.None);
+        }
+    }
+
+
+}
---
+++ src/Otor.MsixHero.DeviceGuardLoginHelper/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Otor.MsixHero.Appx.Signing.DeviceGuard;
+
+namespace Otor.MsixHero.DeviceGuardLoginHelper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void ClickMe(object sender, RoutedEventArgs e)
+        {
+            var dgss = new DgssTokenCreator();
+            var r = await dgss.SignIn(CancellationToken.None).ConfigureAwait(true);
+            this.Label.Text = "access = " + r.AccessToken + System.Environment.NewLine + "refresh = " + r.RefreshToken;
+        }
+    }
+}
