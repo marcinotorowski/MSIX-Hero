e7c95a354ed83598a5f4b190c22f4190748a832f
Marcin Otorowski <marcin.otorowski+git@gmail.com>
2020-11-11 00:49:16 +0100

    Design for the OPEN flyout

---
+++ src/Otor.MsixHero.App/Controls/PackageExpert/Views/Open/Open.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Otor.MsixHero.App.Controls.PackageExpert.Views.Open
+{
+    /// <summary>
+    /// Interaction logic for StoreSource.xaml
+    /// </summary>
+    public partial class Open
+    {
+        public Open()
+        {
+            InitializeComponent();
+        }
+    }
+}
