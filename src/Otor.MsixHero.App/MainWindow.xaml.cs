c59dcf03d1b3e67e82a753abbde29b342bd73f79
Marcin Otorowski <marcin.otorowski+git@gmail.com>
2020-12-03 21:55:55 +0100

    Fixed acryllic effect, maximization and other small issues

--- src/Otor.MsixHero.App/MainWindow.xaml.cs
+++ src/Otor.MsixHero.App/MainWindow.xaml.cs
@@ -1,70 +1,23 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Input;
-using ControlzEx.Behaviors;
-using ControlzEx.Standard;
-using Otor.MsixHero.App.Hero;
-using Otor.MsixHero.Infrastructure.Services;
-using Prism.Modularity;
-using Prism.Services.Dialogs;
-using Interaction = Microsoft.Xaml.Behaviors.Interaction;
+using System.Windows.Shell;
 
 namespace Otor.MsixHero.App
 {
-    internal static class WindowHelper
-    {
-        public static bool IsWindowHandleValid(IntPtr windowHandle)
-        {
-            return windowHandle != IntPtr.Zero
-#pragma warning disable 618
-                   && NativeMethods.IsWindow(windowHandle);
-#pragma warning restore 618
-        }
-    }
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
-        private readonly WindowChromeBehavior behavior;
-
-        public MainWindow(IInteractionService interactionService, IDialogService dialogService, IModuleManager moduleManager)
+        public MainWindow()
         {
             InitializeComponent();
-
-            this.behavior = new WindowChromeBehavior();
-            this.behavior.IgnoreTaskbarOnMaximize = false;
-            this.behavior.TryToBeFlickerFree = true;
-            this.behavior.ResizeBorderThickness = this.WindowState == WindowState.Maximized ? new Thickness(0) : SystemParameters.WindowResizeBorderThickness;
-            Interaction.GetBehaviors(this).Add(this.behavior);
-
-            this.StateChanged += OnStateChanged;
+            this.Loaded += OnLoaded;
         }
 
-        private void OnStateChanged(object sender, EventArgs e)
+        private void OnLoaded(object sender, RoutedEventArgs e)
         {
-            this.behavior.ResizeBorderThickness = this.WindowState == WindowState.Maximized ? new Thickness(0) : SystemParameters.WindowResizeBorderThickness;
-        }
-
-        private void UIElement_OnMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (this.WindowState == WindowState.Maximized)
-            {
-                return;
-            }
-            else
-            {
-                this.DragMove();
-            }
-        }
-
-        private void UIElement_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ClickCount == 2)
-            {
-                this.WindowState = this.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
-            }
+            WindowChrome.GetWindowChrome(this).CaptionHeight = 55;
         }
     }
 }
