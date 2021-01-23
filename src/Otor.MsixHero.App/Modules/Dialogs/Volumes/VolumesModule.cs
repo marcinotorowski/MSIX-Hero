3753fd9bfdb8691aa0ee1092f51fff72d7c2066e
Marcin Otorowski <marcin.otorowski+git@gmail.com>
2021-01-23 23:42:19 +0100

    Updated license to GNU GPL 3

--- src/Otor.MsixHero.App/Modules/Dialogs/Volumes/VolumesModule.cs
+++ src/Otor.MsixHero.App/Modules/Dialogs/Volumes/VolumesModule.cs
@@ -1,4 +1,20 @@
-﻿using Otor.MsixHero.App.Controls;
+﻿// MSIX Hero
+// Copyright (C) 2021 Marcin Otorowski
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// Full notice:
+// https://github.com/marcinotorowski/msix-hero/blob/develop/LICENSE.md
+
+using Otor.MsixHero.App.Controls;
 using Otor.MsixHero.App.Modules.Dialogs.Volumes.ChangeVolume.View;
 using Otor.MsixHero.App.Modules.Dialogs.Volumes.ChangeVolume.ViewModel;
 using Otor.MsixHero.App.Modules.Dialogs.Volumes.NewVolume.View;
--- src/Otor.MsixHero.App/Modules/Main/MainModule.cs
+++ src/Otor.MsixHero.App/Modules/Main/MainModule.cs
@@ -1,4 +1,20 @@
-﻿using System.Windows.Controls;
+﻿// MSIX Hero
+// Copyright (C) 2021 Marcin Otorowski
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// Full notice:
+// https://github.com/marcinotorowski/msix-hero/blob/develop/LICENSE.md
+
+using System.Windows.Controls;
 using Otor.MsixHero.App.Modules.Main.Sidebar.ViewModels;
 using Otor.MsixHero.App.Modules.Main.Sidebar.Views;
 using Otor.MsixHero.App.Modules.Main.Toolbar.ViewModels;
