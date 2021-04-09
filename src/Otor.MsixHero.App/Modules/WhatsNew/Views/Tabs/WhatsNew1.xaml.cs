a1fb787da39a94df5678db11a26a3ba696fcdc27
Marcin Otorowski <marcin.otorowski+git@gmail.com>
2021-04-09 21:36:42 +0200

    Updated release notes

--- src/Otor.MsixHero.App/Modules/WhatsNew/Views/Tabs/WhatsNew1.xaml.cs
+++ src/Otor.MsixHero.App/Modules/WhatsNew/Views/Tabs/WhatsNew1.xaml.cs
@@ -14,13 +14,44 @@
 // Full notice:
 // https://github.com/marcinotorowski/msix-hero/blob/develop/LICENSE.md
 
+using System;
+using System.Windows;
+using Prism.Modularity;
+using Prism.Regions;
+using Prism.Services.Dialogs;
+
 namespace Otor.MsixHero.App.Modules.WhatsNew.Views.Tabs
 {
     public partial class WhatsNew1
     {
-        public WhatsNew1()
+        private readonly IDialogService dialogService;
+        private readonly IRegionNavigationService navigationService;
+        private readonly IModuleManager moduleManager;
+
+        public WhatsNew1(IDialogService dialogService, IRegionNavigationService navigationService, IModuleManager moduleManager)
         {
+            this.dialogService = dialogService;
+            this.navigationService = navigationService;
+            this.moduleManager = moduleManager;
             InitializeComponent();
+        }
+
+        private void GoToDashboard(object sender, RoutedEventArgs e)
+        {
+            this.moduleManager.LoadModule(ModuleNames.Dashboard);
+            this.navigationService.RequestNavigate(new Uri(NavigationPaths.Dashboard, UriKind.RelativeOrAbsolute));
+        }
+
+        private void CreateAppInstaller(object sender, RoutedEventArgs e)
+        {
+            this.moduleManager.LoadModule(ModuleNames.Dialogs.AppInstaller);
+            this.dialogService.ShowDialog(NavigationPaths.DialogPaths.AppInstallerEditor, new DialogParameters(), result => { });
+        }
+
+        private void GoToSettings(object sender, RoutedEventArgs e)
+        {
+            this.moduleManager.LoadModule(ModuleNames.Dialogs.Settings);
+            this.dialogService.ShowDialog(NavigationPaths.DialogPaths.Settings, new DialogParameters(), result => { });
         }
     }
 }
--- src/Otor.MsixHero.App/Modules/WhatsNew/Views/WhatsNewView.xaml.cs
+++ src/Otor.MsixHero.App/Modules/WhatsNew/Views/WhatsNewView.xaml.cs
@@ -14,25 +14,14 @@
 // Full notice:
 // https://github.com/marcinotorowski/msix-hero/blob/develop/LICENSE.md
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Otor.MsixHero.App.Modules.WhatsNew.Views
 {
     /// <summary>
     /// Interaction logic for WhatsNewView.xaml
     /// </summary>
-    public partial class WhatsNewView : UserControl
+    public partial class WhatsNewView
     {
         public WhatsNewView()
         {
